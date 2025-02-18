e93c03a6441141f84df44f3aaebdf849e51f63ca
Anshik01 <anshik0706@gmail.com>
2025-02-18 12:11:40 +0530

    [Anshik Jaiswal] Add. Solution for dependency injection using reflection

---
+++ Day06/src/main/reflection/DependencyInjectionUsingReflection/Main.java
@@ -0,0 +1,13 @@
+package reflection.DependencyInjectionUsingReflection;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            DIContainer container = new DIContainer();
+            Client client = container.getInstance(Client.class);
+            client.doSomething();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Day06/src/main/reflection/DependencyInjectionUsingReflection/Service.java
@@ -0,0 +1,8 @@
+package reflection.DependencyInjectionUsingReflection;
+
+public class Service {
+    public void serve() {
+        System.out.println("Service is serving...");
+
+    }
+}
